a98c04e43529eb8e8c9121e9dc1fdd31b1d911f5
pietaMEDS <maxibanp@gmail.com>
2025-01-22 06:18:50 +0500

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div style={{ width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "5rem" }}><span style={{ color: "green", textDecoration: "none" }}>&gt;</span><span style={{ color: "white", textDecoration: "underline green" }}>Hello World</span></div>
+    <div className="container">
+      <span className="arrow">&gt;</span>
+      <span className="text">Hello World</span>
+    </div>
   );
 }
